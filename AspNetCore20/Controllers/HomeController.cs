18b8bbeafb0d99b64ac68a36315e099f644eaddb
David Ebbo <david.ebbo@gmail.com>
2018-03-20 16:25:14 -0700

    Add class library

--- AspNetCore20/Controllers/HomeController.cs
+++ AspNetCore20/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace AspNetCore20.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = MyClassLibrary.Hello.SayHello("David");
 
             return View();
         }
