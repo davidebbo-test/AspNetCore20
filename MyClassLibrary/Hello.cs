cab7cbdb92561aab8daa3efc951062c61a78b33c
David Ebbo <david.ebbo@gmail.com>
2018-04-17 18:14:35 -0700

    Print the assembly location

--- MyClassLibrary/Hello.cs
+++ MyClassLibrary/Hello.cs
@@ -6,6 +6,6 @@ namespace MyClassLibrary
 {
     public class Hello
     {
-        public static string SayHello(string name) => $"Hello {name}!";
+        public static string SayHello(string name) => $"Hello {name}! ({typeof(Hello).Assembly.Location})";
     }
 }
